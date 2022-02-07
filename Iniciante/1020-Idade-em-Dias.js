2769133ddaa9dabab575bf069ac7d6cf2575a012
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-02-06 22:19:36 -0300

    Solved problems 1020 and 1052

---
+++ Iniciante/1020-Idade-em-Dias.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const idade = parseInt(lines.shift());
+
+console.log(`${Math.trunc(idade / 365)} ano(s)\n${Math.trunc((idade % 365) / 30)} mes(es)\n${Math.trunc((idade % 365) % 30)} dia(s)`);
