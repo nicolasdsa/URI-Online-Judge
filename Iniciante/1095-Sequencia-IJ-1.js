47f644e98e03ca37f21ccd201580d8736fa53fc3
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-03-02 23:16:59 -0300

    Solved problems 1095, 1096 and 1097

---
+++ Iniciante/1095-Sequencia-IJ-1.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let j = 60;
+let i = 1;
+
+while(j >= 0){
+  console.log(`I=${i} J=${j}`);
+  i += 3;
+  j -= 5;
+}
---
+++ Iniciante/1096-Sequencia-IJ-2.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let j = 7;
+let i = 1;
+
+for(i; i <= 9; i += 2){
+  console.log(`I=${i} J=${j}\nI=${i} J=${j - 1}\nI=${i} J=${j - 2}`);
+}
