54914f2e105eb1aa4ad865ce44ed1af32f28f122
nicolas <nicolassosamachado25@gmail.com>
2022-03-17 15:11:10 -0300

    Fixing /dev/ and solved problems 1962, 2006 and 2029

--- Iniciante/1930-Tomadas.js
+++ Iniciante/1930-Tomadas.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 let [T1, T2, T3, T4] = lines.shift().split(" ");
